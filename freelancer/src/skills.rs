25f438cd302881f07d9d9b35e859bae36ede23ca
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-01-18 06:50:49 +0500

    ADD: unique constraint to prevent duplicate values

--- freelancer/src/skills.rs
+++ freelancer/src/skills.rs
@@ -1,11 +1,8 @@
-use serde::{Deserialize, Serialize};
-use axum::{Extension, Json};
-use lib::AppState;
-use axum::{
-    Router,
-    routing::post,
-};
 use axum::http::StatusCode;
+use axum::{Extension, Json};
+use axum::{Router, routing::post};
+use lib::AppState;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize, sqlx::Type)]
 #[sqlx(type_name = "skills_enum", rename_all = "SCREAMING_SNAKE_CASE")]
@@ -44,6 +41,21 @@ pub async fn generate_skill(
     Extension(state): Extension<AppState>,
     Json(payload): Json<Skills>,
 ) -> Result<(), StatusCode> {
+    let findskill = sqlx::query(
+        "
+        SELECT * FROM skills
+        WHERE user_id = $1 AND skill = $2",
+    )
+    .bind(payload.user_id)
+    .bind(payload.skill as SkillsEnum)
+    .fetch_optional(&state.db)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    if findskill.is_some() {
+        return Err(StatusCode::NOT_FOUND);
+    }
+
     sqlx::query(
         "
         INSERT INTO skills
