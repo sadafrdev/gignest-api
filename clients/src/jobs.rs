610615663a5b087083c53530bbafc90fe5625574
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-01-23 07:17:21 +0500

    refactor: update CRUD and add handlers and services

--- clients/src/jobs.rs
+++ clients/src/jobs.rs
@@ -18,60 +18,9 @@ pub struct Job {
     pub budget_max: BigDecimal,
 }
 
-pub async fn create_job(
-    Extension(state): Extension<AppState>,
-    Json(payload): Json<Job>,
-) -> Result<(), StatusCode> {
-    sqlx::query(
-        "
-        INSERT INTO jobs (client_id, title, description, budget_min, budget_max)
-        VALUES ($1, $2, $3, $4, $5)",
-    )
-    .bind(payload.client_id)
-    .bind(&payload.title)
-    .bind(&payload.description)
-    .bind(payload.budget_min)
-    .bind(payload.budget_max)
-    .execute(&state.db)
-    .await
-    .map_err(|e| {
-        eprintln!("SQL ERROR: {:?}", e);
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
-
-    Ok(())
-}
-
 #[derive(Deserialize, Serialize, Debug)]
 pub struct ClientID {
     pub client_id: i64,
-}
-
-pub async fn get_jobs(
-    Extension(state): Extension<AppState>,
-    Json(payload): Json<ClientID>,
-) -> Result<Json<Option<Job>>, StatusCode> {
-    let jobs = sqlx::query_as::<_, Job>(
-        r#"
-        SELECT
-            client_id,
-            title,
-            description,
-            budget_min,
-            budget_max
-        FROM jobs
-        WHERE client_id = $1
-        "#,
-    )
-    .bind(payload.client_id)
-    .fetch_optional(&state.db)
-    .await
-    .map_err(|e| {
-        eprintln!("SQL ERROR: {e:?}");
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
-
-    Ok(Json(jobs))
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -83,67 +32,113 @@ pub struct UpdateJob {
     pub budget_max: BigDecimal,
 }
 
-pub async fn update_job(
-    Extension(state): Extension<AppState>,
-    Json(payload): Json<UpdateJob>,
-) -> Result<StatusCode, StatusCode> {
-    sqlx::query(
-        r#"
-            UPDATE jobs
-            SET
-                title = $1,
-                description = $2,
-                budget_min = $3,
-                budget_max = $4
-            WHERE id = $5
-        "#,
-    )
-    .bind(&payload.title)
-    .bind(&payload.description)
-    .bind(payload.budget_min)
-    .bind(payload.budget_max)
-    .bind(payload.id)
-    .execute(&state.db)
-    .await
-    .map_err(|e| {
-        eprintln!("SQL ERROR: {:?}", e);
-        StatusCode::INTERNAL_SERVER_ERROR
-    });
-
-    Ok(StatusCode::OK)
-}
-
 #[derive(Deserialize, Serialize, Debug)]
 pub struct JobID {
     id: i64,
 }
 
-pub async fn delete_job(
-    Extension(state): Extension<AppState>,
-    Json(payload): Json<JobID>,
-) -> Result<StatusCode, StatusCode> {
-    sqlx::query(
-        r#"
-            DELETE FROM jobs
-            WHERE id = $1
-        "#,
-    )
-    .bind(payload.id)
-    .execute(&state.db)
-    .await
-    .map_err(|e| {
-        eprintln!("SQL ERROR: {:?}", e);
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
+impl Job {
 
-    Ok(StatusCode::NO_CONTENT)
-}
+    pub async fn create_job(
+        Extension(state): Extension<AppState>,
+        Json(payload): Json<Job>,
+    ) -> Result<(), StatusCode> {
+        sqlx::query(
+            "
+            INSERT INTO jobs (client_id, title, description, budget_min, budget_max)
+            VALUES ($1, $2, $3, $4, $5)",
+        )
+        .bind(payload.client_id)
+        .bind(&payload.title)
+        .bind(&payload.description)
+        .bind(payload.budget_min)
+        .bind(payload.budget_max)
+        .execute(&state.db)
+        .await
+        .map_err(|e| {
+            eprintln!("SQL ERROR: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
 
-pub fn router(state: AppState) -> Router {
-    Router::new()
-        .route("/job", post(create_job))
-        .route("/jobs", get(get_jobs))
-        .route("/update-job", put(update_job))
-        .route("/delete-job", delete(delete_job))
-        .layer(Extension(state))
-}
+        Ok(())
+    }
+
+    pub async fn get_jobs(
+        Extension(state): Extension<AppState>,
+        Json(payload): Json<ClientID>,
+    ) -> Result<Option<Job>, StatusCode> {
+        let jobs = sqlx::query_as::<_, Job>(
+            r#"
+            SELECT
+                client_id,
+                title,
+                description,
+                budget_min,
+                budget_max
+            FROM jobs
+            WHERE client_id = $1
+            "#,
+        )
+        .bind(payload.client_id)
+        .fetch_optional(&state.db)
+        .await
+        .map_err(|e| {
+            eprintln!("SQL ERROR: {e:?}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        Ok(jobs)
+    }
+
+    pub async fn update_job(
+        Extension(state): Extension<AppState>,
+        Json(payload): Json<UpdateJob>,
+    ) -> Result<(), StatusCode> {
+        sqlx::query(
+            r#"
+                UPDATE jobs
+                SET
+                    title = $1,
+                    description = $2,
+                    budget_min = $3,
+                    budget_max = $4
+                WHERE id = $5
+            "#,
+        )
+        .bind(&payload.title)
+        .bind(&payload.description)
+        .bind(payload.budget_min)
+        .bind(payload.budget_max)
+        .bind(payload.id)
+        .execute(&state.db)
+        .await
+        .map_err(|e| {
+            eprintln!("SQL ERROR: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        });
+
+        Ok(())
+    }
+
+    pub async fn delete_job(
+        Extension(state): Extension<AppState>,
+        Json(payload): Json<JobID>,
+    ) -> Result<(), StatusCode> {
+        sqlx::query(
+            r#"
+                DELETE FROM jobs
+                WHERE id = $1
+            "#,
+        )
+        .bind(payload.id)
+        .execute(&state.db)
+        .await
+        .map_err(|e| {
+            eprintln!("SQL ERROR: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        Ok(())
+    }
+
+} 
--- clients/src/routes.rs
+++ clients/src/routes.rs
@@ -1,9 +1,10 @@
 use crate::jobs;
 use axum::{Extension, Router};
 use lib::AppState;
+use crate::job_router;
 
 pub fn router(state: AppState) -> Router {
     Router::new()
-        .merge(jobs::router(state.clone()))
+        .merge(job_router(state.clone()))
         .layer(Extension(state))
 }
