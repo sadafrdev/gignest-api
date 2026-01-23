59f71d2f36bb490f22069ca6f021a86b969a4325
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-01-23 18:40:02 +0500

    ADD: handler for generating skill

--- freelancer/src/lib.rs
+++ freelancer/src/lib.rs
@@ -1,3 +1,21 @@
+use lib::AppState;
+use axum::Extension;
+use axum::Json;
 pub mod certificate;
 pub mod routes;
 pub mod skills;
+use skills::{User, Skills};
+use axum::{http::StatusCode, Router, routing::{post,get}};
+
+pub async fn add_skill(
+    Extension(state): Extension<AppState>,
+    Json(payload): Json<Skills>,
+) -> Result<(), StatusCode> {
+    skills::generate_skill(Extension(state), Json(payload)).await
+}
+
+pub fn skills_routes(state: AppState) -> Router {
+    Router::new()
+    .route("/skill", post(add_skill))
+    .layer(Extension(state))
+}
--- freelancer/src/routes.rs
+++ freelancer/src/routes.rs
@@ -1,11 +1,11 @@
 pub use crate::certificate;
-use crate::skills;
 use axum::{Extension, Router};
 use lib::AppState;
+use crate::skills_routes;
 
 pub fn router(state: AppState) -> Router {
     Router::new()
         .merge(certificate::router(state.clone()))
-        .merge(skills::router(state.clone()))
+        .merge(skills_routes(state.clone()))
         .layer(Extension(state))
 }
