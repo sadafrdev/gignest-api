30ef08fd35d7ae9013099f74ddf48e32d293c20e
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-01-23 19:22:48 +0500

    Refactore: added update and delete handler

--- freelancer/src/lib.rs
+++ freelancer/src/lib.rs
@@ -7,9 +7,9 @@ pub mod skills;
 use axum::{
     Router,
     http::StatusCode,
-    routing::{get, post},
+    routing::{delete, get, post, put},
 };
-use skills::{Skills, User};
+use skills::{SkillID, Skills, UpdateSkill, User};
 
 pub async fn add_skill(
     Extension(state): Extension<AppState>,
@@ -25,9 +25,25 @@ pub async fn get_skills(
     Skills::get_skills(Extension(state), Json(payload)).await
 }
 
+pub async fn update_skill(
+    Extension(state): Extension<AppState>,
+    Json(payload): Json<UpdateSkill>,
+) -> Result<(), StatusCode> {
+    Skills::update_skill(Extension(state), Json(payload)).await
+}
+
+pub async fn delete_skill(
+    Extension(state): Extension<AppState>,
+    Json(payload): Json<SkillID>,
+) -> Result<(), StatusCode> {
+    Skills::delete_skill(Extension(state), Json(payload)).await
+}
+
 pub fn skills_routes(state: AppState) -> Router {
     Router::new()
         .route("/skill", post(add_skill))
         .route("/skills", get(get_skills))
+        .route("/update-skill", put(update_skill))
+        .route("/delete-skill", delete(delete_skill))
         .layer(Extension(state))
 }
