106204cfdd80bd45ad69e04e400a95ef8f4aa508
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-02-25 03:38:37 +0500

    Refactore: add unique in db and get hash password from db

--- lib/authentication/src/login.rs
+++ lib/authentication/src/login.rs
@@ -1,10 +1,8 @@
 use axum::{Json, extract::Extension, http::StatusCode};
 use serde::Deserialize;
 use utils::db::AppState;
-use argon2::{Argon2, password_hash::{SaltString, PasswordHasher}};
-use rand_core::OsRng;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Login {
     pub email: String,
     pub password: String,
@@ -15,22 +13,15 @@ impl Login {
         Extension(state): Extension<AppState>,
         Json(payload): Json<Self>,
     ) -> Result<(), StatusCode> {
-        let salt = SaltString::generate(&mut OsRng);
-        let hashed_password = Argon2::default()
-            .hash_password(payload.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
-
         let res = sqlx::query(
             r#"
                 SELECT
-                    password, email
+                   password
                 FROM users
-                WHERE email = $1 AND password = $2
+                WHERE email = $1 
             "#,
         )
         .bind(payload.email)
-        .bind(hashed_password)
         .fetch_optional(&state.db)
         .await
         .map_err(|e| {
@@ -38,9 +29,8 @@ impl Login {
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
 
-        match res {
-            Some(_) => Ok(()),
-            None => Err(StatusCode::NOT_FOUND),
-        }
+        println!("RES: {:?}", res);
+        
+        Ok(())
     }
 }
--- migrations/20251221234405_init.sql
+++ migrations/20251221234405_init.sql
@@ -34,7 +34,7 @@ CREATE TABLE educations (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT REFERENCES users(id),
   country country NOT NULL,
-  degree TEXT NOT NULL,
+  degree TEXT NOT NULL UNIQUE,
   institute TEXT NOT NULL,
   major TEXT NOT NULL,
   year_of_graduation DATE NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE educations (
 CREATE TABLE languages (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT REFERENCES users(id),
-  language language NOT NULL,
+  language language NOT NULL UNIQUE,
   language_level language_level NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -52,7 +52,7 @@ CREATE TABLE languages (
 CREATE TABLE certificates (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT REFERENCES users(id),
-  name TEXT NOT NULL,
+  name TEXT NOT NULL UNIQUE,
   certificate_by TEXT NOT NULL,
   year DATE NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
@@ -68,13 +68,13 @@ CREATE TABLE portfolios (
 CREATE TABLE skills (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT REFERENCES users(id),
-  skill skills_enum NOT NULL
+  skill skills_enum NOT NULL UNIQUE
 );
 
 CREATE TABLE jobs (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   client_id BIGINT REFERENCES users(id),
-  title TEXT NOT NULL,
+  title TEXT NOT NULL UNIQUE,
   description TEXT NOT NULL,
   budget_min NUMERIC NOT NULL,
   budget_max NUMERIC NOT NULL,
