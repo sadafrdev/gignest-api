19842a330dd33e6913c08e82b70fb2b879c63163
Sadaf Raza <sadaf.r.dev@gmail.com>
2026-09-07 02:58:26 +0500

    changed the port

--- api/src/main.rs
+++ api/src/main.rs
@@ -14,7 +14,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let connection = establish_connection().await?;
     let app = routes::router(connection);
 
-    let addr: SocketAddr = "127.0.0.1:3000".parse().unwrap();
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr: SocketAddr = format!("0.0.0.0:{}", port).parse().unwrap();
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("🚀 http://{}", addr);
